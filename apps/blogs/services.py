cc2fe72b061424d703ea438bab9d1708369c51e1
verafadeeva <lunatik.yar@gmail.com>
2023-11-05 16:42:41 +0300

    Add transaction to services

--- apps/blogs/services.py
+++ apps/blogs/services.py
@@ -1,15 +1,19 @@
 import uuid
 
+from django.db import transaction
+
 from apps.blogs.models import Post
 from apps.blogs.selectors import get_post
 from apps.users.models import CustomUser
 
 
+@transaction.atomic
 def create_post(data: dict, user: CustomUser) -> Post:
     post, is_created = Post.objects.get_or_create(author=user, **data)
     return (post, is_created)
 
 
+@transaction.atomic
 def update_post(id: uuid, data: dict) -> Post:
     post = get_post(id=id)
     for key, value in data.items():
@@ -18,6 +22,7 @@ def update_post(id: uuid, data: dict) -> Post:
     return post
 
 
+@transaction.atomic
 def delete_post(id: uuid) -> None:
     post = get_post(id=id)
     post.delete()
