9091c6daae02b6164609b924fb8300b539cfe75c
verafadeeva <lunatik.yar@gmail.com>
2023-11-05 16:01:29 +0300

    Fix permission

--- apps/api/v1/permissions.py
+++ apps/api/v1/permissions.py
@@ -3,8 +3,10 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAuthorOrIsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
         return (
